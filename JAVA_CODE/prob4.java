9500c1ff4c3f6e5b6e7c2b838dc90d18fc26c5a8
Kazi Abu Jafar Anik <kaziabujafaranik@gmail.com>
2026-03-02 21:12:55 +0600

    Add files via upload

---
+++ JAVA_CODE/prob4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class prob4{
+    public static void main(String[] args){
+        int[] value = new int[3];
+        Scanner input = new Scanner(System.in);
+        for(int i = 0 ; i < 3 ; i++){
+            value[i] = input.nextInt();
+        }
+        System.out.println(Math.max(value[0],Math.max(value[1], value[2])));
+    }
+}
