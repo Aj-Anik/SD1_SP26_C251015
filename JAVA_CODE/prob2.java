936dabe07b2c3b273607ab253ea04b8502e92103
Kazi Abu Jafar Anik <kaziabujafaranik@gmail.com>
2026-03-02 20:47:42 +0600

    Add files via upload

---
+++ JAVA_CODE/prob2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner ;
+class prob2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
